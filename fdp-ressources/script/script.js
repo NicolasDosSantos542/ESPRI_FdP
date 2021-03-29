2eacd79251df70f3b2b28042013fc5ab351173b4
NicolasDosSantos <nicolas.de-meyere-dos-santos@epitech.eu>
2021-03-29 11:44:26 +0200

    rename to index.html changement script to vanillaJS

--- fdp-ressources/script/script.js
+++ fdp-ressources/script/script.js
@@ -1,37 +1,17 @@
-$(document).ready(function() {
-    $(".plus").click(function () {
-        
-        $(this).next().html(`<input class="input"  type="text" placeholder="écrivez ici">`).focus()
-        $('.input').focus();
-        $('input').keypress(function(e){
-            if( e.which === 13 ){
-         
-                $(this).parent().html($(this).val())
-                $(this).remove()
-            }
-        });
-        
-    })
-    
-    
-    $(".plus-liste").click(function () {
-        
-        $(this).next().append(`<input class="input"  type="text" placeholder="écrivez ici">`).focus()
-        $('.input').focus();
-        $('input').keypress(function(e){
-            if( e.which === 13 ){
-                
-                $(this).parent().append("<li>-"+$(this).val()+"</li>")
-                $(this).remove()
-            }
-        });
-        
-    })
-    $(".saison-pastille").click(function () {
-        let bob = prompt("quel est votre score?")
-        $(this).children().html(bob)
-    
-    })
-    
+window.onload = function () {
+    let plus = document.getElementsByClassName('plus');
 
-})
+    
+    
+    const plusFunction = function () {
+        console.log(this)
+        this.closest('p').innerHTML = '<input class="input"  type="text" placeholder="écrivez ici">`';
+    };
+    
+    for (let i = 0; i < plus.length; i++) {
+        plus[i].addEventListener('click', plusFunction, false);
+    }
+
+
+
+}
---
+++ fdp-ressources/script/scriptJQ.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    $(".plus").click(function () {
+        
+        $(this).next().html(`<input class="input"  type="text" placeholder="écrivez ici">`).focus()
+        $('.input').focus();
+        $('input').keypress(function(e){
+            if( e.which === 13 ){
+         
+                $(this).parent().html($(this).val())
+                $(this).remove()
+            }
+        });
+        
+    })
+    
+    
+    $(".plus-liste").click(function () {
+        
+        $(this).next().append(`<input class="input"  type="text" placeholder="écrivez ici">`).focus()
+        $('.input').focus();
+        $('input').keypress(function(e){
+            if( e.which === 13 ){
+                
+                $(this).parent().append("<li>-"+$(this).val()+"</li>")
+                $(this).remove()
+            }
+        });
+        
+    })
+    $(".saison-pastille").click(function () {
+        let bob = prompt("quel est votre score?")
+        $(this).children().html(bob)
+    
+    })
+    
+
+})
